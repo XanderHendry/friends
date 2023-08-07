7fa639e48a6b8449cba532b5f4597b81ec268c0d
Xander Hendry <vhendry.coding@gmail.com>
2023-08-07 15:01:23 -0400

    Add method array < 60

--- friends.rb
+++ friends.rb
@@ -1,13 +1,12 @@
 friends = [1, 10, 95, 12, 55, 40, 50, 99]
 
 
-# friends.each do |friend|
-#     if friend > 30 
-#         puts friend
-#     end   
-# end  
+friends.each do |friend|
+    if friend > 30 
+        puts friend
+    end
+    if friend < 60 
+        puts friend
+    end 
+end  
 
-friends.select do |friend|
-    friends = friend > 30
-    puts friends
-end
